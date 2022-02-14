d4065af8f5050c3c15794dc4c528c53596604dea
monizb <46259712+monizb@users.noreply.github.com>
2022-02-14 09:57:11 +0530

    binded port

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get("/OSLeaderboard", (req, res) => {
     });
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000');
 });
